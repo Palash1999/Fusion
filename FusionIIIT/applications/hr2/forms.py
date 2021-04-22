916e9d9cd36e07ce9406082a4fa132573685c037
Pranav Choudhary <2018184@iiitdmj.ac.in>
2021-04-22 18:15:28 +0530

    Forms edited

--- FusionIIIT/applications/hr2/forms.py
+++ FusionIIIT/applications/hr2/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Employee,EmpConfidentialDetails,ForeignService
+from .models import Employee, EmpConfidentialDetails, ForeignService
+
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -10,37 +11,34 @@ class EditDetailsForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['extra_info', 'father_name', 'mother_name', 'religion', 'category',
-                  'cast', 'home_state', 'home_district', 'date_of_joining', 'designation','blood_group']
-    
+                  'cast', 'home_state', 'home_district', 'date_of_joining', 'designation', 'blood_group']
+
         widgets = {
-            'date_of_joining':DateInput()
+            'date_of_joining': DateInput()
         }
-    
+
     def __init__(self, *args, **kwargs):
         super(EditDetailsForm, self).__init__(*args, **kwargs)
-     
 
 
 class EditConfidentialDetailsForm(forms.ModelForm):
 
     class Meta:
         model = EmpConfidentialDetails
-        fields = ['extra_info', 'aadhar_no', 
+        fields = ['extra_info', 'aadhar_no',
                   'maritial_status', 'bank_account_no', 'salary']
 
     def __init__(self, *args, **kwargs):
-       super(EditConfidentialDetailsForm, self).__init__(*args, **kwargs)
+        super(EditConfidentialDetailsForm, self).__init__(*args, **kwargs)
 
-                
 
 class EditServiceBookForm(forms.ModelForm):
 
     class Meta:
         model = ForeignService
         fields = ['extra_info', 'start_date', 'end_date', 'job_title', 'organisation',
-                  'description', 'salary_source', 'designation','service_type']
-        widgets = {'start_date': DateInput(),'end_date':DateInput()}
+                  'description', 'salary_source', 'designation', 'service_type']
+        widgets = {'start_date': DateInput(), 'end_date': DateInput()}
 
     def __init__(self, *args, **kwargs):
-       super(EditServiceBookForm, self).__init__(*args, **kwargs)
-             
+        super(EditServiceBookForm, self).__init__(*args, **kwargs)
--- FusionIIIT/applications/hr2/views.py
+++ FusionIIIT/applications/hr2/views.py
@@ -3,7 +3,7 @@ from .models import *
 from applications.globals.models import ExtraInfo
 from django.db.models import Q
 from django.http import Http404
-from .forms import EditDetailsForm,EditConfidentialDetailsForm, EditServiceBookForm
+from .forms import EditDetailsForm, EditConfidentialDetailsForm, EditServiceBookForm
 from django.contrib import messages
 from applications.eis.models import *
 
@@ -16,133 +16,141 @@ from applications.eis.models import *
 #         return render(request,template,{'temps':temps})
 
 
+def edit_employee_details(request, id):
+    """ Views for edit details"""
+    template = 'hr2Module/editDetails.html'
 
-def edit_employee_details(request,id):
-        """ Views for edit details"""
-        template='hr2Module/editDetails.html'
+    try:
+        employee = ExtraInfo.objects.get(pk=id)
+    except:
+        raise Http404("Post does not exist")
 
-        try:
-               employee = ExtraInfo.objects.get(pk=id)
-        except:
-                raise Http404("Post does not exist")
+    if request.method == "POST":
+        form = EditDetailsForm(request.POST, request.FILES)
+        conf_form = EditConfidentialDetailsForm(request.POST, request.FILES)
 
-        
+        if form.is_valid() and conf_form.is_valid():
+            form.save()
+            conf_form.save()
+            messages.success(request, "Employee details edited successfully")
+        else:
+            if not form.is_valid():
+                print("Error in form")
+            if not conf_form.is_valid():
+                print("Error in conffrom")
+            messages.warning(request, "Unable to proceed")
+            pass
 
-        if request.method == "POST":
-                form = EditDetailsForm(request.POST, request.FILES)
-                conf_form = EditConfidentialDetailsForm(request.POST,request.FILES)
-
-                
-                if form.is_valid() and conf_form.is_valid():
-                        form.save()
-                        conf_form.save()
-                        messages.success(request, "Employee details edited successfully")
-                else:
-                        pass
-
-
-        form = EditDetailsForm(initial={'extra_info': employee.id})
-        conf_form = EditConfidentialDetailsForm(initial={'extra_info': employee})
-        context = {'form': form,'confForm':conf_form,'employee':employee
+    form = EditDetailsForm(initial={'extra_info': employee.id})
+    conf_form = EditConfidentialDetailsForm(initial={'extra_info': employee})
+    context = {'form': form, 'confForm': conf_form, 'employee': employee
                }
 
-        return render(request,template, context)
-
+    return render(request, template, context)
 
 
 def hr_admin(request):
-        """ Views for HR2 Admin page """
-        template='hr2Module/hradmin.html'
+    """ Views for HR2 Admin page """
+    template = 'hr2Module/hradmin.html'
 
-        # searched employee
-        query = request.GET.get('search')
-        
-        if(request.method == "GET"):
-                if(query!=None):
-                        emp = ExtraInfo.objects.filter(
-                                Q(user__first_name__icontains=query)|
-                                Q(user__last_name__icontains=query)
-                                
+    # searched employee
+    query = request.GET.get('search')
 
-                        ).distinct()
-                        emp  = emp.filter(user_type="faculty")
-                else:
-                        emp = ExtraInfo.objects.all()
-                        emp  = emp.filter(user_type="faculty")
+    if(request.method == "GET"):
+        if(query != None):
+            emp = ExtraInfo.objects.filter(
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+
+
+            ).distinct()
+            emp = emp.filter(user_type="faculty")
         else:
-                emp = ExtraInfo.objects.all()
-                emp  = emp.filter(user_type="faculty")
+            emp = ExtraInfo.objects.all()
+            emp = emp.filter(user_type="faculty")
+    else:
+        emp = ExtraInfo.objects.all()
+        emp = emp.filter(user_type="faculty")
 
-        context = {'emps':emp}
-        return render(request,template,context)
+    context = {'emps': emp}
+    return render(request, template, context)
+
 
 def service_book(request):
-        """
-        Views for service book page
-        """
-        user = request.user
-        extra_info = ExtraInfo.objects.select_related().get(user=user)
-        lien_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "LIEN").order_by('-start_date')
-        deputation_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "DEPUTATION").order_by('-start_date')
-        other_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "OTHER").order_by('-start_date')
-        appraisal_form = EmpAppraisalForm.objects.filter(extra_info = extra_info).order_by('-year')
-        pf = extra_info.id
-        empprojects = emp_research_projects.objects.filter(pf_no=pf).order_by('-start_date')
-        visits = emp_visits.objects.filter(pf_no=pf).order_by('-entry_date')
-        conferences = emp_confrence_organised.objects.filter(pf_no=pf).order_by('-date_entry')
-        template = 'hr2Module/servicebook.html'
-        awards = emp_achievement.objects.filter(pf_no=pf).order_by('-date_entry')
-        thesis = emp_mtechphd_thesis.objects.filter(pf_no=pf).order_by('-date_entry')
-        context = {'lienServiceBooks':lien_service_book,'deputationServiceBooks':deputation_service_book,'otherServiceBooks':other_service_book,
-        'appraisalForm':appraisal_form,
-        'empproject':empprojects,
-        'visits':visits,
-        'conferences':conferences,
-        'awards':awards,
-        'thesis':thesis,
-        }
-
-        return render(request,template,context)
-
-
-def view_employee_details(request,id):
-        """ Views for edit details"""
-        extra_info = ExtraInfo.objects.get(pk=id)
-        lien_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "LIEN").order_by('-start_date')
-        deputation_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "DEPUTATION").order_by('-start_date')
-        other_service_book = ForeignService.objects.filter(extra_info = extra_info).filter(service_type= "OTHER").order_by('-start_date')
-        appraisal_form = EmpAppraisalForm.objects.filter(extra_info = extra_info).order_by('-year')        
-        template = 'hr2Module/viewdetails.html'
-        context = {'lienServiceBooks':lien_service_book,'deputationServiceBooks':deputation_service_book,'otherServiceBooks':other_service_book,'user':extra_info.user,'extrainfo':extra_info,'appraisalForm':appraisal_form
-       }
-        return render(request,template,context)
-
-def edit_employee_servicebook(request,id):
-        """ Views for edit Service Book details"""
-        template='hr2Module/editServiceBook.html'
-
-        try:
-               employee = ExtraInfo.objects.get(pk=id)
-        except:
-                raise Http404("Post does not exist")
-
-        
-
-        if request.method == "POST":
-                form = EditServiceBookForm(request.POST, request.FILES)
-             
-
-                
-                if form.is_valid():
-                        form.save()
-                        messages.success(request, "Employee Service Book details edited successfully")
-                else:
-
-                        pass
-
-
-        form = EditServiceBookForm(initial={'extra_info': employee.id})
-        context = {'form': form,'employee':employee
+    """
+    Views for service book page
+    """
+    user = request.user
+    extra_info = ExtraInfo.objects.select_related().get(user=user)
+    lien_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="LIEN").order_by('-start_date')
+    deputation_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="DEPUTATION").order_by('-start_date')
+    other_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="OTHER").order_by('-start_date')
+    appraisal_form = EmpAppraisalForm.objects.filter(
+        extra_info=extra_info).order_by('-year')
+    pf = extra_info.id
+    empprojects = emp_research_projects.objects.filter(
+        pf_no=pf).order_by('-start_date')
+    visits = emp_visits.objects.filter(pf_no=pf).order_by('-entry_date')
+    conferences = emp_confrence_organised.objects.filter(
+        pf_no=pf).order_by('-date_entry')
+    template = 'hr2Module/servicebook.html'
+    awards = emp_achievement.objects.filter(pf_no=pf).order_by('-date_entry')
+    thesis = emp_mtechphd_thesis.objects.filter(
+        pf_no=pf).order_by('-date_entry')
+    context = {'lienServiceBooks': lien_service_book, 'deputationServiceBooks': deputation_service_book, 'otherServiceBooks': other_service_book,
+               'appraisalForm': appraisal_form,
+               'empproject': empprojects,
+               'visits': visits,
+               'conferences': conferences,
+               'awards': awards,
+               'thesis': thesis,
                }
 
-        return render(request,template, context)
+    return render(request, template, context)
+
+
+def view_employee_details(request, id):
+    """ Views for edit details"""
+    extra_info = ExtraInfo.objects.get(pk=id)
+    lien_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="LIEN").order_by('-start_date')
+    deputation_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="DEPUTATION").order_by('-start_date')
+    other_service_book = ForeignService.objects.filter(
+        extra_info=extra_info).filter(service_type="OTHER").order_by('-start_date')
+    appraisal_form = EmpAppraisalForm.objects.filter(
+        extra_info=extra_info).order_by('-year')
+    template = 'hr2Module/viewdetails.html'
+    context = {'lienServiceBooks': lien_service_book, 'deputationServiceBooks': deputation_service_book, 'otherServiceBooks': other_service_book, 'user': extra_info.user, 'extrainfo': extra_info, 'appraisalForm': appraisal_form
+               }
+    return render(request, template, context)
+
+
+def edit_employee_servicebook(request, id):
+    """ Views for edit Service Book details"""
+    template = 'hr2Module/editServiceBook.html'
+
+    try:
+        employee = ExtraInfo.objects.get(pk=id)
+    except:
+        raise Http404("Post does not exist")
+
+    if request.method == "POST":
+        form = EditServiceBookForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Employee Service Book details edited successfully")
+        else:
+            messages.warning(request, "Unable to proceed")
+            pass
+
+    form = EditServiceBookForm(initial={'extra_info': employee.id})
+    context = {'form': form, 'employee': employee
+               }
+
+    return render(request, template, context)
